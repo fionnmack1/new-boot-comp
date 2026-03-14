4541b6f3369a3e02ba246b7e10499bfe75424c1d
fionnmack1 <fionnmcgillycuddy@icloud.com>
2026-03-14 22:01:55 +0000

    Add Footer component with copyright notice

--- components/Footer.js
+++ components/Footer.js
@@ -9,3 +9,12 @@ export default function Footer() {
     </footer>
   );
 }
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-10 text-center">
+      <p>&copy; 2023 Football Boot Comparison. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
