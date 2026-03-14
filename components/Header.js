26a593d86ad34b3189303db279a66af89137e818
fionnmack1 <fionnmcgillycuddy@icloud.com>
2026-03-14 22:01:35 +0000

    Refactor Header component structure and formatting

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,20 @@ export default function Header() {
       </div>
     </header>
   );
-}
+} const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4 flex justify-between">
+      <div className="text-xl">Logo</div>
+      <nav>
+        <ul className="flex space-x-4">
+          <li>Boots</li>
+          <li>Compare</li>
+          <li>Reviews</li>
+          <li>Blog</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
