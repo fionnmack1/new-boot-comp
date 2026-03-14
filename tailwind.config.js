b5bf5a30764e4b1920d060400984bac86e7b516b
fionnmack1 <fionnmcgillycuddy@icloud.com>
2026-03-14 21:58:05 +0000

    Update Tailwind CSS configuration file

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,3 +10,11 @@ npx create-next-app@latest football-boot-comparison
 cd football-boot-comparison
 npm install -D tailwindcss postcss autoprefixer
 npx tailwindcss init -p
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
